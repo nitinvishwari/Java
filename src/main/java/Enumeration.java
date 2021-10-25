368279e58ac9a254cb9ef1c283b9ede796a40b14
nitinvishwari <nitinvishwari@gmail.com>
2021-10-25 18:29:45 -0400

    Enumeration implementation to get integer from String value

--- src/main/java/Enumeration.java
+++ src/main/java/Enumeration.java
@@ -1,5 +1,46 @@
-public class Enumeration {
-    public static void main(String[] args) {
+import java.util.HashMap;
 
+public class Enumeration {
+    public enum Weekdays{
+        Monday("Monday", 0),
+        Tuesday("Tuesday",1),
+        Wednesday("Wednesday",2),
+        Thursday("Thursday",3),
+        Friday("Friday",4),
+        Saturday("Saturday",5),
+        Sunday("Sunday",6);
+
+        private String stringValue;
+        private int value;
+
+        static HashMap<String, Integer> hm = new HashMap<>();
+
+        static{
+            for(Weekdays w: values()){
+                hm.put(w.getStringValue(), w.getValue());
+            }
+        }
+
+        Weekdays(String i, int j) {
+            this.stringValue = i;
+            this.value = j;
+        }
+
+        private int getValue(){
+            return this.value;
+        }
+
+        private String getStringValue(){
+            return this.stringValue;
+        }
+
+        public static int getDayNumber(String s){
+            return hm.getOrDefault(s, -1);
+        }
+    }
+
+    public static void main(String[] args) {
+        String string = "Wednesday";
+        System.out.println(Weekdays.getDayNumber(string));
     }
 }
---
+++ src/main/java/EnumerationImp2.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class EnumerationImp2 {
+    public enum Weekdays{
+        Monday( 0),
+        Tuesday(1),
+        Wednesday(2),
+        Thursday(3),
+        Friday(4),
+        Saturday(5),
+        Sunday(6);
+
+        private int value;
+
+        Weekdays(int i) {
+            this.value = i;
+        }
+
+        private int getValue(){
+            return this.value;
+        }
+    }
+
+    public static void main(String[] args) {
+        String string = "Tuesday";
+        System.out.println(Weekdays.valueOf(string).value);
+    }
+}
